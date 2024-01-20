3b098808c49b24b77ac2f65e39e233a80d05a375
pedrot111 <pedrochincolla11@gmail.com>
2024-01-20 16:29:31 -0300

    added file for centralized data storage

---
+++ src/app/data/data.ts
@@ -0,0 +1,72 @@
+export const DATA = {
+    name: 'Pedro Chincolla',
+    role: 'Full Stack Developer',
+    location: 'In Copenaghen, Denmark',
+    icon_location: '/assets/icons/denmark1.png',
+    contact: [
+        {
+            name: 'mail',
+            icon: 'bi bi-envelope-at-fill',
+            href: '',
+            
+        },
+        {
+            name: 'github',
+            icon: 'bi bi-github',
+            href: 'https://github.com/PedroT111',
+            
+        },
+        {
+            name: 'whatsapp',
+            icon: 'bi bi-whatsapp',
+            href: '',
+            
+        },
+        {
+            name: 'linkedin',
+            icon: 'bi bi-linkedin',
+            href: 'https://www.linkedin.com/in/pedro-chincolla/',
+            
+        }
+    ],
+    about: 'As a programmer, I stand out for propelling my career with a fervent passion for crafting innovative technological solutions. Currently, I work mostly with Angular, Node.js, SQL, and MongoDB. I seek challenges that stimulate creativity, generate a transformative impact, and allow me to contribute to innovative development.',
+    jobs: [
+        {
+            company: 'Freelance',
+            role: 'Full Stack Developer',
+            start: 'Apr 2021',
+            end: 'Now',
+            tasks: [
+                {
+                    description: 'Frontend development of a cryptocurrency website. Utilized technologies such as React, Bootstrap, HTML5, and CSS3 to create an engaging user interface.'
+                },
+                {
+                    description: 'Developed "MiCafeApp," a website designed to enhance the coffee shop purchasing experience, particularly focusing on the "Take Away" mode. Employed technologies including Angular, Bootstrap, Node.js, and MongoDB to bring the project to fruition.',
+                }
+            ]
+        },
+        {
+            company: 'Accounting Firm',
+            role: 'Administrative Assistant',
+            start: 'Sep 2020',
+            end: 'Nov 2022',
+            tasks: [
+                {
+                    description: 'Responsible for the preparation of financial statements, accounting, tax settlement, and payroll.'
+                },
+            ]
+        },
+        {
+            company: 'Alkemy',
+            role: 'JavaScript Developer',
+            start: 'Jul 2021',
+            end: 'Nov 2021',
+            tasks: [
+                {
+                    description: 'Development of a website using the following technologies: React, Node.js, Express, MongoDB, Jira.'
+                },
+            ]
+        }
+    ]
+
+}
