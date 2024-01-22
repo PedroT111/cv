ea8b09ec07b474bc3c4690d6dcbba00ce8c02fcc
pedrot111 <pedrochincolla11@gmail.com>
2024-01-22 17:59:16 -0300

    implements routing for home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MicafeappPageComponent } from './pages/micafeapp-page/micafeapp-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  //{path: 'micafeapp', component: MicafeappPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
