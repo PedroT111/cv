2557ecf67cbf5206dcafc5032cd6cce76fee49ae
pedrot111 <pedrochincolla11@gmail.com>
2024-03-29 11:10:28 +0100

    add project data

---
+++ src/app/data/miCafeApp.ts
@@ -0,0 +1,35 @@
+export const MI_CAFE_DATA = {
+    summary: "MiCafeApp is the perfect solution to modernize and streamline the café experience. With our application, users can order their favorite coffee from the comfort of their mobile device, without the need to wait in long lines. Simply select your preferred products, schedule the pickup time, and you're all set!", 
+    technologies: ['Angular', 'Nodejs', 'TypeScript', 'Express', 'MongoDB', 'Bootstrap'],
+    app_image_url: '',
+    summary_2: "For coffee lovers who are always on the go, MiCafeApp offers the convenience of planning their orders in advance, ensuring their coffee is ready and waiting for them upon arrival at the café.",
+    admin_tool_description: 'Additionally, MiCafeApp provides cafés with the necessary tools to efficiently manage their business. From inventory management and creating offers and discounts, to tracking orders and generating detailed reports, our platform helps optimize operations and enhance customer satisfaction.',
+    admin_tool_sections: [
+        {
+            title: 'Products',
+            img: ['admin1.png'],
+            description: "Allows you to add, edit, or delete products from the café's inventory. This tool facilitates product management, including price updates, image uploading, and category organization."
+        },
+        {
+            title: 'Orders:',
+            img: ['admin7.png'],
+            description: 'Manage and monitor all orders placed by customers, including viewing, editing, and tracking the status of pending, in-progress, and completed orders. Additionally, notify customers about changes in the status of their orders.'
+        },
+        {
+            title: 'Offers',
+            img: ['admin5.png'],
+            description: "Create and manage special offers to attract customers, such as seasonal promotions and special combos. This tool allows you to configure offer conditions and monitor their performance. Additionally, it offers the ability to schedule automatic offers based on specific conditions to maximize promotion effectiveness."
+        },
+        {
+            title: 'Discounts',
+            img: ['admin4.png'],
+            description: "Configure and manage discounts applicable to products in the inventory. This tool enables you to customize offers according to business needs. Additionally, it offers the ability to segment discounts by customer groups to enhance customer loyalty."
+        },
+        {
+            title: 'Reports',
+            img: ['admin1.png'],
+            description: "Access detailed reports that provide insights into business performance. This includes data on frequent customers, sales analysis over time periods, and employee performance metrics. Additionally, you'll be able to generate custom reports to tailor analysis to specific business needs."
+        }
+    ]
+    
+}
